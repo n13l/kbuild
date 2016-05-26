6d57186d058e34be9551dc93bf2bf9cc043d6103
Daniel Kubec <niel@rtfm.cz>
2016-05-26 01:55:45 -0400

    Update archiver without arch.

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -2,4 +2,4 @@
 #files:=$(find . -regex ".*\.\(dll\|so\|dylib\|exe\)")
 files=$(find obj/ -executable -type f)
 arch=$(file obj/sysconfig | cut -f2 -d",")
-echo "$files" | xargs tar -czvf $arch-$1
+echo "$files" | xargs tar -czvf $1
