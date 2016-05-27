ca18226c7416a73b11d6735a64ddceaa30b27faf
Daniel Kubec <niel@rtfm.cz>
2016-05-27 11:28:00 -0400

    Improved CI archives.

--- scripts/archive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#files:=$(find . -regex ".*\.\(dll\|so\|dylib\|exe\)")
-files=$(find obj/ -executable -type f)
-arch=$(file obj/sysconfig | cut -f2 -d",")
-echo "$files" | xargs tar -czvf $1
--- scripts/travis/version.sh
+++ scripts/travis/version.sh
@@ -4,9 +4,9 @@ export BUILD_MINOR=0
 export BUILD_REVISION=1
 
 num=`echo $1 | cut -d. -f1`
-if [[ $num = *[[:digit:]]* ]]; then export BUILD_MAJOR=$num ; fi
+if [ $num = *[[:digit:]]* ]; then export BUILD_MAJOR=$num ; fi
 num=`echo $1 | cut -d. -f2`
-if [[ $num = *[[:digit:]]* ]]; then export BUILD_MINOR=$num ; fi
+if [ $num = *[[:digit:]]* ]; then export BUILD_MINOR=$num ; fi
 num=`echo $1 | cut -d. -f3`
-if [[ $num = *[[:digit:]]* ]]; then export BUILD_REVISION=$num ; fi
+if [ $num = *[[:digit:]]* ]; then export BUILD_REVISION=$num ; fi
 echo "release: $BUILD_MAJOR $BUILD_MINOR $BUILD_REVISION"
