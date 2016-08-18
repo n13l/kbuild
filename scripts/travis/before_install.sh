8185ccb91cdd89d36e406db2471f07a9ee390b4d
Daniel Kubec <niel@rtfm.cz>
2016-08-17 19:29:46 -0700

    Fixed PowerPC64 gcc-5 build.

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -76,8 +76,8 @@ fi
 if [ "$BUILD_ARCH" == "powerpc64" ]; then
   echo "deb http://ftp.de.debian.org/debian sid main contrib non-free" | sudo tee -a /etc/apt/sources.list
   sudo apt-get update -qq
-  ls -la /usr/
   sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes -o Dpkg::Options::="--force-overwrite" install flex bison gperf pkg-config gcc-5-powerpc64-linux-gnu binutils-powerpc64-linux-gnu qemu-user-static binfmt-support
+  sudo ln -s /usr/bin/powerpc64-linux-gnu-gcc-5 /usr/bin/powerpc64-linux-gnu-gcc
   unset CC
   export CROSS_COMPILE=powerpc64-linux-gnu-
   export OS_EXEC="echo"
